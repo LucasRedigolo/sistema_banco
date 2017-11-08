d88354500d0e6b1c190e6dc7c1c5ae6323513558
LUCAS GOMES REDIGOLO <43692939876@jpmorgan.sp>
2017-11-08 20:41:25 -0200

    aula dia 08/11

--- Dados/ContaPessoaJuridica.cs
+++ Dados/ContaPessoaJuridica.cs
@@ -1,23 +1,49 @@
 using System;
+using Util;
 
-namespace Dados
-{
-    public class ContaPessoaJuridica : Conta
-    {
+namespace Dados {
+    public class ContaPessoaJuridica : Conta {
         public Empresa empresa { get; set; }
         /// <summary>
         /// Efetua saque na conta pessoa física mais 50 centavos
         /// </summary>
         /// <param name="Valor">parametro do tipo double com o valor do saque</param>
         /// <returns>Retorna o saldo</returns>
-        public override double Sacar(double Valor    ){
+        public override double Sacar (double Valor) {
             this.Saldo -= (Valor + 0.50);
             return this.Saldo;
         }
+        /// <summary>
+        /// Efetua o cadastro de uma conta de PJ
+        /// </summary>
+        public void CadastrarConta () {
+            Console.WriteLine ("Digite a Razao Social");
+            string razaosocial = Console.ReadLine ();
 
-        
-        public void CadastrarConta(string Numero, string Agencia, string Conta, string RazaoSocial, DateTime DataAbertura, string CNPJ){
+            Console.WriteLine ("Digite a data de abertura");
+            string dataabertura = Console.ReadLine ();
 
+            Validacao validacao = new Validacao ();
+            bool cnpjvalido = false;
+
+            do {
+                Console.WriteLine ("Digite seu cnpj");
+                string cnpj = Console.ReadLine ();
+                cnpjvalido = validacao.ValidarCNPJ (cnpj);
+
+                if (cnpjvalido != true) {
+                    Console.WriteLine ("Cnpj inválido");
+                }
+            } while (cnpjvalido != true);
+
+            System.Console.WriteLine ("Digite o Numero da Conta: ");
+            string numeroConta = Console.ReadLine ();
+
+            System.Console.WriteLine ("Digite o número da agencia:");
+            string numeroAgencia = Console.ReadLine ();
+
+            System.Console.WriteLine ("Digite o Saldo:");
+            double saldo = Convert.ToDouble (Console.ReadLine ());
         }
     }
 }
--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -1,57 +1,33 @@
 ﻿using System;
 using Dados;
 using Util;
-namespace Terminal
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+using NetOffice.ExcelApi;
+
+namespace Terminal {
+    class Program {
+        static void Main (string[] args) {
 
             string opcao = "";
-            
+
             //Menu principal 
-            do{
-                Console.WriteLine("Digite a opção");
-                Console.WriteLine("1 - Cadastrar Conta");
-                Console.WriteLine("2 - Sacar");
-                Console.WriteLine("3 - Depositar");
-                Console.WriteLine("9 - Sair");
-                
-                opcao = Console.ReadLine();
+            do {
+                Console.WriteLine ("Digite a opção");
+                Console.WriteLine ("1 - Cadastrar Conta");
+                Console.WriteLine ("2 - Sacar");
+                Console.WriteLine ("3 - Depositar");
+                Console.WriteLine ("9 - Sair");
 
-                switch (opcao)
-                {
+                opcao = Console.ReadLine ();
+
+                switch (opcao) {
                     case "1":
-                    {
-                        CadastrarConta();
-                        break;
-                    }
+                        {
+                            ContaPessoaJuridica contajur = new ContaPessoaJuridica ();
+                            contajur.CadastrarConta ();
+                            break;
+                        }
                 }
-            }while(opcao != "9");
-        }
-
-        static void CadastrarConta(){
-            Console.WriteLine("Digite a Razao Social");
-            string razaosocial = Console.ReadLine();
-
-            Console.WriteLine("Digite a data de abertura");
-            string dataabertura = Console.ReadLine();
-            
-            Validacao validacao = new Validacao();
-            bool cnpjvalido = false;
-
-            do{
-                Console.WriteLine("Digite seu cnpj");
-                string cnpj = Console.ReadLine();
-                cnpjvalido = validacao.ValidarCNPJ(cnpj);
-
-                if(cnpjvalido != true){
-                    Console.WriteLine("Cnpj inválido");
-                }
-            }while(cnpjvalido != true);
-
-
+            } while (opcao != "9");
         }
     }
 }
